75c96c5f2b090b990ad9d11866ebf80698b61775
Sidewinder53 <19623985+Sidewinder53@users.noreply.github.com>
2019-01-19 15:46:53 +0100

    Improved launcher behaviour

--- run.bat
+++ run.bat
@@ -1,18 +1,21 @@
-@echo off
-@echo ################################################################################
-@echo #                     Win10EnterpriseReady, starting up...                     #
-@echo ################################################################################
-@echo:
+echo off
+echo ################################################################################
+echo #                     Win10EnterpriseReady, starting up...                     #
+echo ################################################################################
+echo:
 ping 127.0.0.1 -n 2 >nul
 <nul set /p =Searching for script file... 
-if exist "Win10EnterpriseReady_0.2.ps1" (
+echo.
+if exist "Win10EnterpriseReady.ps1" (
 	@echo Found!
 	<nul set /p =Bypassing Microsoft PowerShell Execution Policy...
-	powershell.exe -noprofile -executionpolicy bypass -file .\Win10EnterpriseReady_0.2.ps1
+	C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noprofile -executionpolicy bypass -file .\Win10EnterpriseReady.ps1
 	@echo Done!
     exit
 ) else (
-	@echo File missing. Opening download page...
+	echo.
+	echo FAILURE: File missing. Opening download page.
+	timeout /T 5
 	start "" https://github.com/Sidewinder53/Win10EnterpriseReady
     exit
 )
