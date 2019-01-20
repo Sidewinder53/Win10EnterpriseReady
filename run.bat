1b331ea72901661ceeaebc7e28ddedbef799d58c
Sidewinder53 <19623985+Sidewinder53@users.noreply.github.com>
2019-01-20 18:40:32 +0100

    Improved launcher console output

--- run.bat
+++ run.bat
@@ -1,15 +1,14 @@
 @echo off
 echo ################################################################################
-echo #                     Win10EnterpriseReady, starting up...                     #
+echo #         Win10EnterpriseReady Launcher, preparing script execution...         #
 echo ################################################################################
 echo:
 ping 127.0.0.1 -n 2 >nul
 <nul set /p =Searching for script file... 
-echo.
 if exist "Win10EnterpriseReady.ps1" (
 	@echo Found!
 	<nul set /p =Bypassing Microsoft PowerShell Execution Policy...
-	C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noprofile -executionpolicy bypass -file .\Win10EnterpriseReady.ps1
+	powershell.exe -noprofile -executionpolicy bypass -file .\Win10EnterpriseReady.ps1
 	@echo Done!
     exit
 ) else (
